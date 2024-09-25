05f9428b83b218789c356cab49d352b7dd67e1fa
Arka Prava Basu <arkaprava94@gmail.com>
2024-09-25 19:21:41 +0530

    Stub out all static ObjectPool usages in MoveViewJob
    Signed-off-by: Arka Prava Basu <arkaprava94@gmail.com>

--- MPChartLib/src/main/java/com/github/mikephil/charting/jobs/MoveViewJob.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/jobs/MoveViewJob.java
@@ -12,26 +12,26 @@ import com.github.mikephil.charting.utils.ViewPortHandler;
  */
 public class MoveViewJob extends ViewPortJob {
 
-    private static ObjectPool<MoveViewJob> pool;
+//    private static ObjectPool<MoveViewJob> pool;
+//
+//    static {
+//        pool = ObjectPool.create(2, new MoveViewJob(null,0,0,null,null));
+//        pool.setReplenishPercentage(0.5f);
+//    }
 
-    static {
-        pool = ObjectPool.create(2, new MoveViewJob(null,0,0,null,null));
-        pool.setReplenishPercentage(0.5f);
-    }
+//    public static MoveViewJob getInstance(ViewPortHandler viewPortHandler, float xValue, float yValue, Transformer trans, View v){
+//        MoveViewJob result = pool.get();
+//        result.mViewPortHandler = viewPortHandler;
+//        result.xValue = xValue;
+//        result.yValue = yValue;
+//        result.mTrans = trans;
+//        result.view = v;
+//        return result;
+//    }
 
-    public static MoveViewJob getInstance(ViewPortHandler viewPortHandler, float xValue, float yValue, Transformer trans, View v){
-        MoveViewJob result = pool.get();
-        result.mViewPortHandler = viewPortHandler;
-        result.xValue = xValue;
-        result.yValue = yValue;
-        result.mTrans = trans;
-        result.view = v;
-        return result;
-    }
-
-    public static void recycleInstance(MoveViewJob instance){
-        pool.recycle(instance);
-    }
+//    public static void recycleInstance(MoveViewJob instance){
+//        pool.recycle(instance);
+//    }
 
     public MoveViewJob(ViewPortHandler viewPortHandler, float xValue, float yValue, Transformer trans, View v) {
         super(viewPortHandler, xValue, yValue, trans, v);
@@ -46,11 +46,12 @@ public class MoveViewJob extends ViewPortJob {
         mTrans.pointValuesToPixel(pts);
         mViewPortHandler.centerViewPort(pts, view);
 
-        recycleInstance(this);
+//        recycleInstance(this);
     }
 
     @Override
     protected ObjectPool.Poolable instantiate() {
-        return new MoveViewJob(mViewPortHandler, xValue, yValue, mTrans, view);
+        throw new IllegalStateException("Don't use");
+//        return new MoveViewJob(mViewPortHandler, xValue, yValue, mTrans, view);
     }
 }
